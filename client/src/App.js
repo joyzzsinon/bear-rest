a33e6097ed0a60fd667c6e1baa14c43e00fb4193
Sinon <j-sinon@hotmail.com>
2018-04-24 11:23:19 +0700

    list bears

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,25 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class App extends Component {
+  constructor(props){
+     super(props)
+     this.state = {data: []};
+      }
+      componentDidMount(){
+      axios.get('http://localhost:8000/api/bears')
+        .then(result => {
+        this.setState({data: result.data})
+        })
+      }
   render() {
+    let bears = this.state.data;
     return (
-      <div>Hello Saturn</div>
+      <div>
+        {
+          brears.map(brear => <div key={bears.id}>{bear.name}</div>)
+        }
+        </div>
     );
   }
 }
